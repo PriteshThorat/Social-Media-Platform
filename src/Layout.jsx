c1c881492733e996e6920b22c885619bb8120e2d
Pritesh Thorat <thoratpritesh1@gmail.com>
2025-10-28 18:34:41 +0530

    fix: simplify ScrollRestoration component usage in Layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -10,7 +10,7 @@ const Layout = () => {
         <>
             {!isProfilePage && <Header/>}
             <Outlet />
-            <ScrollRestoration getKey={(location) => location.pathname} />
+            <ScrollRestoration />
         </>
     );
 };
