242c31ccb3cf56a9bda6638754b173f20a84a977
Pritesh Thorat <thoratpritesh1@gmail.com>
2025-11-02 16:31:46 +0530

    feat: add vite-sitemap plugin and update sitemap configuration for improved SEO

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from 'vite-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    sitemap({
+      baseURL: 'ttps://social-media-platform-seven-orpin.vercel.app', // Replace with your actual domain
+      // Optionally, specify URLs manually if not all routes are automatically discoverable
+      urls: ['/profile'],
+    }),
+  ],
   server: {
     historyApiFallback: true // Ensure correct routing in dev
   }
