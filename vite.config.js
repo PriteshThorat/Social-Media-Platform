e61834f643b0db60fb9900e244d07b585104a0e2
Pritesh Thorat <thoratpritesh1@gmail.com>
2025-12-08 00:31:25 +0530

    fix: comment out sitemap plugin configuration for future implementation

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
-import sitemap from 'vite-plugin-sitemap';
+// TODO: Add sitemap in production
+// import sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
     tailwindcss(),
-    sitemap({
+    /*sitemap({
       hostname: 'https://social-media-platform-seven-orpin.vercel.app', // full domain required
       routes: ['/'],   // add all your routes
       exclude: ['/login', '/signup', '/forgot-password', '/change-avatar'],
-    }),
+    }),*/
   ],
   server: {
     historyApiFallback: true // Ensure correct routing in dev
