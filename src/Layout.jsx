d6fb030b1b25c9318a64ab3718f72b2d5b2a7e17
Pritesh Thorat <thoratpritesh1@gmail.com>
2025-12-11 23:31:38 +0530

    feat: refactor Layout and Header components for improved readability and structure

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,17 +1,20 @@
-import { Header } from './components/index';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Header } from "./components/index";
+import { Outlet, useLocation } from "react-router-dom";
 
 const Layout = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-    const isProfilePage = location.pathname.startsWith("/profile/");
+  const isProfilePage = location.pathname.startsWith("/profile/");
 
-    return (
-        <>
-            {!isProfilePage && <Header/>}
-            <Outlet />
-        </>
-    );
+  return (
+    <>
+      {!isProfilePage && <Header />}
+      {/* Add left margin for desktop sidebar */}
+      <div className="lg:ml-64 xl:ml-72">
+        <Outlet />
+      </div>
+    </>
+  );
 };
 
 export default Layout;
